dffbe677a7dfe95b52b87e04f082ad9ef6974cb9
Vinh TTN <nocontact@gmail.com>
2024-07-21 11:45:40 +1000

    New way to show npc Voky TrieuMan

---
+++ server1/script/activitysys/config/801/config.lua
@@ -0,0 +1,18 @@
+Include("\\script\\activitysys\\config\\801\\head.lua")
+tbConfig = {}
+tbConfig[1] = --Ò»¸öÏ¸½Ú
+{
+	nId = 1,
+	szMessageType = "ServerStart",
+	szName = "creatSimcityDialogNpc",
+	nStartDate = nil,
+	nEndDate  = nil,
+	tbMessageParam = {nil},
+	tbCondition = 
+	{
+	},
+	tbActition = 
+	{
+		{"ThisActivity:InitAddNpc",	{nil} },
+	},
+}
---
+++ server1/script/activitysys/config/801/head.lua
@@ -0,0 +1,18 @@
+Include("\\script\\global\\vinh\\main.lua")
+Include("\\script\\activitysys\\activity.lua")
+
+pActivity = ActivityClass:new()
+pActivity.nId = 801
+pActivity.szName = "Simcity"
+pActivity.nStartDate = 202400000000
+pActivity.nEndDate = 302400010000
+pActivity.szDescription = "nil"
+pActivity.nGroupId = nil
+pActivity.nVersion = 5
+
+ 
+
+function pActivity:InitAddNpc()
+    auto_add_npc_vinh()
+end
+ 
